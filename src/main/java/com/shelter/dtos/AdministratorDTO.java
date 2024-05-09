e8c9ea0310e3454428831cb7ab28b54ac67cea42
Lena Loncarevic <l.loncarevic-int@levi9.com>
2024-05-09 22:37:20 +0200

    admin dto

---
+++ src/main/java/com/shelter/dtos/AdministratorDTO.java
@@ -0,0 +1,16 @@
+package com.shelter.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdministratorDTO {
+
+    private Long id;
+
+    private String username;
+
+}
